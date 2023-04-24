eda78a748ff8dbc3c35a0674adf6bee06831fbd1
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-24 23:02:26 +0200

    fixing problem with deleting tasks

--- to-do-app/src/components/Task.js
+++ to-do-app/src/components/Task.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Task = (props) => {
-  const { text, date } = props.task;
+  const { text, date, id } = props.task;
   return (
     <div>
       <p>
         <strong>{text}</strong> - do <span>{date} </span>
         <button onClick={() => props.delete(id)}>Done</button>
-        <button onClick={()=>props.change(id)}>X</button>
+        <button onClick={()=> props.change(id)}>X</button>
       </p>
     </div>
   );
