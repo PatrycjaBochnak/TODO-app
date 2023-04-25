d0df8a0f309895c39b870b0ac08db175997dbb40
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-25 21:25:32 +0200

    creating function of done tasks

--- to-do-app/src/components/TaskList.js
+++ to-do-app/src/components/TaskList.js
@@ -3,18 +3,24 @@ import Task from "./Task"
 
 const TaskList = (props) => {
 
-  const tasks = props.tasks.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
-  const activeTasks = props.tasks.filter(task => task.active === true)
+  const active = props.tasks.filter(task => task.active === true)
+
+  const completed = props.tasks.filter(task => task.active === false)
+
+  const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+  
+  const completedTasks = completed.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change}/>)
+
   return (
     <>
   <div className="active">
     <h2>Tasks to do</h2>
-    {tasks}
+    {activeTasks}
     </div>
     <hr/>
     <div className="done">
       <h2>Done tasks</h2>
-
+    {completedTasks}
     </div>
     </>
   );
