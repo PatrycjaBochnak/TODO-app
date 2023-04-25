fdbca53298bbb2bba9c82ab088d3a5232ea5920e
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-25 23:36:46 +0200

    adding heading site of webpage

--- to-do-app/src/components/AddTask.js
+++ to-do-app/src/components/AddTask.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 
 class AddTask extends Component {
-    state = {  } 
-    render() { 
+    state = {
+        text: '',
+        checked: false,
+        date: new Date().toLocaleString().slice(0, 10),
+    } 
+    render() {
+        const minDate = new Date().toLocaleString().slice(0, 10)
+
+        let maxDate = "2023-12-31"
+    
         return (
             <div className='form'>
-            <div>Add task</div>
+           <input type='text' placeholder='add task' value={this.state.text}/>
+           <input type='checkbox' checked={this.state.checked} id='important'/>
+           <label htmlFor='important'/>Priority
+           <label htmlFor='date'/>Complete task until:
+           <input type='date' value={this.state.date} min={minDate} max={maxDate}/>
+           <button>Add task</button>
             <hr />
             </div>
         );
--- to-do-app/src/components/Task.js
+++ to-do-app/src/components/Task.js
@@ -1,16 +1,33 @@
 import React from "react";
 
 const Task = (props) => {
-  const { text, date, id } = props.task;
+
+  const importantStyle = {
+    color: 'red',
+  }
+
+  const { text, date, id, active, importantTask, finishDate } = props.task;
+
+  if(active) {
+
   return (
     <div>
       <p>
-        <strong>{text}</strong> - do <span>{date} </span>
+        <strong style={importantTask ? importantStyle : null}>{text}</strong> - Do it until: <span>{date} </span>
         <button onClick={() => props.change(id)}>Done</button>
         <button onClick={()=> props.delete(id)}>X</button>
       </p>
     </div>
-  );
+  );} else {
+    const finishedTime = new Date(finishDate).toLocaleString()
+    return (
+      <div>
+         <p>
+        <strong style={importantTask ? importantStyle : null}>{text}</strong> Do it until: <span>{date}<span> Done in:</span> {finishedTime}</span>
+        <button onClick={()=> props.delete(id)}>X</button>
+      </p>
+      </div>
+  )}
 };
 
 export default Task;
