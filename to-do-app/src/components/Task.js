8bc0eedf062cda4554bda685ba1e8f0c3571ed25
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-04-28 12:25:43 +0200

    added alerts to done tasks

--- to-do-app/src/components/Task.js
+++ to-do-app/src/components/Task.js
@@ -5,24 +5,65 @@ const Task = (props) => {
     color: "red",
   };
   const handleClickAlert = () => {
-    const message = document.createElement("div");
-    message.textContent = "you're awesome!";
-    message.style.position = "fixed";
-    message.style.top = "20px";
-    message.style.left = "50%";
-    message.style.transform = "translate(-50%, 0)";
-    message.style.backgroundColor = "black";
-    message.style.color = "white";
-    message.style.padding = "10px";
-    message.style.borderRadius = "5px";
-    message.style.zIndex = "9999";
-    document.body.appendChild(message);
-    
-    setTimeout(() => {
-      document.body.removeChild(message);
-      message.style.transition = "textContent 0.5s ease-out";
-    }, 1000);
+    const message1 = document.createElement("div");
+    message1.textContent = "you're awesome!";
+    message1.style.position = "fixed";
+    message1.style.top = "20px";
+    message1.style.left = "50%";
+    message1.style.transform = "translate(-50%, 0)";
+    message1.style.backgroundColor = "black";
+    message1.style.color = "white";
+    message1.style.padding = "10px";
+    message1.style.borderRadius = "5px";
+    message1.style.zIndex = "9999";
+
+    const message2 = document.createElement("div");
+    message2.textContent = "keep up the good work!";
+    message2.style.position = "fixed";
+    message2.style.top = "60px";
+    message2.style.left = "50%";
+    message2.style.transform = "translate(-50%, 0)";
+    message2.style.backgroundColor = "black";
+    message2.style.color = "white";
+    message2.style.padding = "10px";
+    message2.style.borderRadius = "5px";
+    message2.style.zIndex = "9999";
+
+    const message3 = document.createElement("div");
+    message3.textContent = "incredible! ";
+    message3.style.position = "fixed";
+    message3.style.top = "100px";
+    message3.style.left = "50%";
+    message3.style.transform = "translate(-50%, 0)";
+    message3.style.backgroundColor = "black";
+    message3.style.color = "white";
+    message3.style.padding = "10px";
+    message3.style.borderRadius = "5px";
+    message3.style.zIndex = "9999";
+
+    const random = Math.random();
+
+    if (random < 0.33) {
+      document.body.appendChild(message1);
+      setTimeout(() => {
+        document.body.removeChild(message1);
+        message1.style.transition = "textContent 0.5s ease-out";
+      }, 1000);
+    } else if (random < 0.66) {
+      document.body.appendChild(message2);
+      setTimeout(() => {
+        document.body.removeChild(message2);
+        message2.style.transition = "textContent 0.5s ease-out";
+      }, 1000);
+    } else {
+      document.body.appendChild(message3);
+      setTimeout(() => {
+        document.body.removeChild(message3);
+        message3.style.transition = "textContent 0.5s ease-out";
+      }, 1000);
+    }
   };
+
   const { text, date, id, active, finishDate, important } = props.task;
 
   if (active) {
@@ -31,15 +72,18 @@ const Task = (props) => {
         <p>
           <strong style={important ? importantStyle : null}>{text}</strong> - Do
           it until: <span>{date} </span>
-          <button onClick={() => {
-            props.change(id);
-            handleClickAlert()}}>
-              Done
-              </button>
+          <button
+            onClick={() => {
+              props.change(id);
+              handleClickAlert();
+            }}
+          >
+            Done
+          </button>
           <button
             onClick={() => {
               props.delete(id);
-              }}
+            }}
           >
             X
           </button>
