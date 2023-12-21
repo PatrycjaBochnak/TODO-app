b29fb1b3cb53375557a88b79f31c409a28de34e2
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-12-21 21:28:08 +0100

    component: added navtask

--- src/components/NavTask.js
+++ src/components/NavTask.js
@@ -1,8 +1,35 @@
 import React from "react";
-
+import "../styles/NavTask.css"
 function NavTask() {
+    const handleClickAllTasks = () => {
+        document.getElementById("all-tasks").style.display = 'block';
+    }
+    const handleClickActiveTasks = () => {
+        document.getElementById("active-tasks").style.display = 'block';
+    }
+
+    const handleClickCompletedTasks = () => {
+        document.getElementById("done").style.display = 'block';
+    }
     return(
-        <div>NavTask</div>
+        <div className="nav-task">
+        <div classname="tasks-left-todo">x tasks left</div>
+        <button className="all-tasks-btn"
+            onClick={() => {
+                handleClickAllTasks();
+            }}
+          >All</button>
+                 <button className="active-tasks-btn"
+            onClick={() => {
+                handleClickActiveTasks();
+            }}
+          >Active</button>
+                 <button className="completed-tasks-btn"
+            onClick={() => {
+                handleClickCompletedTasks();
+            }}
+          >Completed</button>
+        </div>
     )
 }
 
