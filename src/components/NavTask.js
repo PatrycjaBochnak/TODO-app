22a1be1020f5c2414e471c3dc9c97687ad94263c
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2023-12-23 22:48:10 +0100

    button: add a button functions

--- src/components/NavTask.js
+++ src/components/NavTask.js
@@ -2,43 +2,53 @@ import React, { useState, useEffect } from "react";
 import "../styles/NavTask.css";
 
 const NavTask = (props) => {
-  const [activeTasks, setActiveTasks] = useState("");
-  const [completedTasks, setCompletedTasks] = useState("");
+  const [activeTasks, setActiveTasks] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
+  const [displayedTasks, setDisplayedTasks] = useState([]);
 
   useEffect(() => {
     const active = props.tasks.filter((task) => task.active === true);
     const completed = props.tasks.filter((task) => task.active === false);
 
-    const activeTasksText = active.map((task) => task.text).join(", ");
-    const completedTasksText = completed.map((task) => task.text).join(", ");
-
-    setActiveTasks(activeTasksText);
-    setCompletedTasks(completedTasksText);
+    setActiveTasks(active);
+    setCompletedTasks(completed);
+    setDisplayedTasks(active); // Domyślnie wyświetlaj aktywne zadania
   }, [props.tasks]);
+
+  const handleActiveButtonClick = () => {
+    setDisplayedTasks(activeTasks);
+  };
+
+  const handleAllButtonClick = () => {
+    setDisplayedTasks([...activeTasks, ...completedTasks]);
+  };
+
+  const handleCompletedButtonClick = () => {
+    setDisplayedTasks(completedTasks);
+  };
 
   return (
     <div className="nav-task">
       <div className="tasks-left">
         {props.tasks.filter((task) => task.active).length} tasks left
       </div>
-      <button
-        className="all-tasks-btn"
-        onClick={() => props.changeTaskToShow(activeTasks)}
-      >
+      <button className="all-tasks-btn" onClick={handleAllButtonClick}>
         All
       </button>
-      <button
-        className="active-tasks-btn"
-        onClick={() => props.changeTaskToShow(activeTasks)}
-      >
+      <button className="active-tasks-btn" onClick={handleActiveButtonClick}>
         Active
       </button>
       <button
         className="completed-tasks-btn"
-        onClick={() => props.changeTaskToShow(completedTasks)}
+        onClick={handleCompletedButtonClick}
       >
         Completed
       </button>
+      <div className="displayed-tasks">
+        {displayedTasks.map((task) => (
+          <div key={task.id}>{task.text}</div>
+        ))}
+      </div>
     </div>
   );
 };
