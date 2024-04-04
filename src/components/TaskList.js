e0d87a44045ce934570259e5f884105df6185049
PatrycjaBochnak <103588081+PatrycjaBochnak@users.noreply.github.com>
2024-04-04 17:13:37 +0200

    adding zip fastener

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "../styles/TaskList.css";
 import Task from "./Task";
 
 const TaskList = (props) => {
   const [activeTasks, setActiveTasks] = useState([]);
-  const [completedTasks, setCompletedTasks] = useState([]);
+  const [showScrollbar, setShowScrollbar] = useState(false);
+  const activeTasksRef = useRef(null);
 
   useEffect(() => {
     const active = props.tasks.filter((task) => task.active === true);
-    const completed = props.tasks.filter((task) => task.active === false);
-
     setActiveTasks(active);
-    setCompletedTasks(completed);
   }, [props.tasks]);
+
+  useEffect(() => {
+    if (activeTasksRef.current.scrollHeight > activeTasksRef.current.clientHeight) {
+      setShowScrollbar(true);
+    } else {
+      setShowScrollbar(false);
+    }
+  }, [activeTasks]);
 
   return (
     <div className="tasks-list">
-      <div className="active-tasks">
+      <div className="active-tasks" ref={activeTasksRef} style={{ maxHeight: "350px", overflowY: "auto" }}>
         {activeTasks.length > 0 ? (
           activeTasks.map((task) => (
             <Task key={task.id} task={task} change={props.change} />
@@ -25,7 +31,17 @@ const TaskList = (props) => {
           <p> You finished your tasks. Great job!</p>
         )}
       </div>
-      <hr />
+      {showScrollbar && (
+        <input
+          type="range"
+          className="scrollbar"
+          min="0"
+          max={activeTasksRef.current.scrollHeight - activeTasksRef.current.clientHeight}
+          onChange={(e) => {
+            activeTasksRef.current.scrollTop = e.target.value;
+          }}
+        />
+      )}
       {/* <div className="done">
         <h2 className="App-headings">Done tasks</h2>
         {completedTasks.length > 5 && <span>Preview for only 5 last tasks</span>}
